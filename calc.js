4cb2c3f06f615153ce669ad010d514da68ceda7e
Ankur-dev95 <machaankur259@gmail.com>
2024-12-28 00:07:57 +0000

    nd

---
+++ calc.js
@@ -0,0 +1,35 @@
+function get1(){
+    return "1"
+}
+function get2(){
+    return "2"
+}
+function get3(){
+    return "3"
+}function get4(){
+    return "4"
+}
+function get5(){
+    return "5"
+}
+function get6(){
+    return "6"
+}function get7(){
+    return "7"
+}function get8(){
+    return "8"
+}function get9(){
+    return "9"
+}
+function get0(){
+    return "0"
+}
+var num1 = "";
+var num2 = "";
+
+
+function add(firstNum,secondNum){
+    const answer = parseInt(firstNum) + parseInt(secondNum);
+
+
+}
